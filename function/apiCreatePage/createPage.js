7598a8b3c336e4caf6d91f508eb9664ee6cc1a92
andrey <abs@yandex.ru>
2025-04-07 22:37:37 +0300

    refact

--- function/apiCreatePage/createPage.js
+++ function/apiCreatePage/createPage.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const path = require('path'); // Импортируем модуль 'path' для работы с путями
 const { saveFilterHTML } = require('../subsidiary/deleteAllScaner');
-const { removeSidebarNav } = require('../subsidiary/deleteNavBlock');
 const { findMaxNumberInFilenames } = require('./additionalFunctions');
 const { extractContextsFromChatPrompts } = require('../apiScan/scan');
 
+/**
+ * Обертка для асинхронного вызова createPage.  Определяет следующий доступный номер файла
+ * и вызывает createPage с этим номером.
+ *
+ * @async
+ * @function createPageWrapper
+ * @param {string} filePath - Путь к исходному HTML файлу, который будет обрабатываться.
+ * @param {string} scanDerectory - Директория, в которой производится сканирование существующих файлов. Используется для определения следующего доступного номера.
+ * @param {string} outputDir - Директория, в которую будут сохранены отфильтрованные HTML файлы.
+ * @throws {Error} Если происходит ошибка при определении максимального номера файла или при создании страниц, ошибка пробрасывается дальше для обработки.
+ * @returns {Promise<void>} - Promise, который разрешается, когда создание страниц завершено.  Void, так как возвращает ошибку если таковая есть.
+ */
 async function createPageWrapper(filePath, scanDerectory, outputDir) { // Обертка для асинхронного вызова
     try {
       const initialFileNumber = await findMaxNumberInFilenames(scanDerectory);
@@ -16,6 +27,17 @@ async function createPageWrapper(filePath, scanDerectory, outputDir) { // Обе
   }
 
 
+/**
+ * Асинхронно создает  HTML файлы на основе извлеченных контекстов из исходного файла.
+ *
+ * @async
+ * @function createPage
+ * @param {string} filePath - Путь к исходному HTML файлу, из которого будут извлечены контексты.
+ * @param {string} outputDir - Директория, в которую будут сохранены отфильтрованные HTML файлы.
+ * @param {number} initialFileNumber - Начальный номер файла для генерации имен новых файлов.
+ * @throws {Error} Если происходит ошибка при извлечении контекстов или сохранении отфильтрованных HTML файлов, ошибка пробрасывается дальше для обработки.
+ * @returns {Promise<Array<any>>} - Promise, который разрешается с массивом извлеченных контекстов (`arrayResult`) после успешного создания и сохранения всех файлов.
+ */
 async function createPage(filePath, outputDir, initialFileNumber) {
 
 
